81290c29004ff8bf330b713acb1dbcde74fdc693
Ian-Mburu <ianongaruiya@gmail.com>
2025-05-27 22:40:05 +0300

    serializers & views

--- backend/campusconnect/serializers.py
+++ backend/campusconnect/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers #type: ignore
 from . models import *
 
 
-class UserSerializer(serializers.modelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     profile_url = serializers.HyperlinkedIdentityField(
         view_name='userprofile-detail',
         lookup_field='username',
@@ -17,7 +17,7 @@ class UserSerializer(serializers.modelSerializer):
 
 class UserProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
-    Skills = serializers.slugRelatedField(
+    Skills = serializers.SlugRelatedField(
         many=True,
         slug_field='name',
         queryset=Skill.objects.all(),
@@ -27,3 +27,37 @@ class UserProfileSerializer(serializers.ModelSerializer):
         model = UserProfile
         fields = '__all__'
         read_only_fields = ['user']
+
+class PostSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+    likes_count = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Post
+        fields = [
+            'id', 'user', 'content_type', 'text_content',
+            'image', 'video', 'created_at', 'likes_count',
+            'comments_count'
+        ]
+        read_only_fields = ['user']
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+
+    def get_comments_count(self, obj):
+        return obj.comments.count()
+
+class GroupSerializer(serializers.ModelSerializer):
+    members = UserSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Group
+        fields = '__all__'
+        read_only_fields = ['creator']
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notification
+        fields = '__all__'
+        read_only_fields = ['user', 'created_at']
--- backend/campusconnect/views.py
+++ backend/campusconnect/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions # type: ignore
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'username'
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Notification.objects.filter(user=self.request.user)
